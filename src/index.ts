72432fe12b3843e6d145aa1de346636246601b8f
JupiterPi <jupiterpi@outlook.de>
2024-04-22 14:48:20 +0200

    optionally read credentials from .filen-cli-credentials

--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,18 @@ if (args["--help"] || args["_"].length == 0) {
         tmpPath: path.join(os.tmpdir(), "filen-cli")
     })
 
-    out("Please enter your Filen credentials:")
-    const email = await prompt("Email: ")
-    const password = await prompt("Password: ")
-    if (!email || !password) errExit("Please provide your credentials!")
-    await filen.login({ email, password })
-    out("")
+    if (fs.existsSync(".filen-cli-credentials")) {
+        const lines = fs.readFileSync(".filen-cli-credentials").toString().split("\n")
+        if (lines.length < 2) errExit("Invalid .filen-cli-credentials!")
+        await filen.login({email: lines[0], password: lines[1]})
+    } else {
+        out("Please enter your Filen credentials:")
+        const email = await prompt("Email: ")
+        const password = await prompt("Password: ")
+        if (!email || !password) errExit("Please provide your credentials!")
+        await filen.login({ email, password })
+        out("")
+    }
 
     if (args["_"][0] == "interactive") {
         out("Interactive console:")
