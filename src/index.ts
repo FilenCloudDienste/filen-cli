92da2062553195ee4ad73f740bb03da7535b5a09
JupiterPi <jupiterpi@outlook.de>
2024-04-22 12:20:49 +0200

    arguments parsing and packaging setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+import arg from "arg"
+
+const args = arg({
+    // arguments
+    "--help": Boolean,
+
+    // aliases
+    "-h": "--help"
+})
+
+if (args["--help"] || args["_"].length == 0) {
+    console.log("Filen CLI v0.0.1")
+}
