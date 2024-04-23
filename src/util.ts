9c8825ca2768f5581e8c1e612ae2b94588492466
JupiterPi <jupiterpi@outlook.de>
2024-04-23 11:34:01 +0200

    fixed formatting

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,8 @@
 // see https://stackoverflow.com/a/19448513
 export function formatTimestamp(ms: number) {
-	const pad2 = (n: number) => { return n < 10 ? '0' + n : n }
+	const pad2 = (n: number) => {
+		return n < 10 ? "0" + n : n
+	}
 	const date = new Date(ms)
 	return date.getFullYear().toString() + "-" + pad2(date.getMonth() + 1) + "-" + pad2( date.getDate()) + " " + pad2( date.getHours() ) + ":" + pad2( date.getMinutes() ) + ":" + pad2( date.getSeconds() ) + "." + pad2( date.getMilliseconds() )
 }
