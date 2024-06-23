2d4ded08871e0bc6c43ee26c0456f15b7e50b730
JupiterPi <jupiterpi@outlook.de>
2024-06-23 20:50:04 +0200

    fix: don't update when release exists but binaries are not attached yet

--- src/updater.ts
+++ src/updater.ts
@@ -20,43 +20,43 @@ export class Updater {
 	 * Check for updates and prompt the user on whether to update.
 	 */
 	public async checkForUpdates(verbose: boolean): Promise<void> {
+		if (version === "0.0.0") {
+			if (verbose) out("Skipping updates in development environment")
+			return
+		}
+
 		const releaseInfoResponse = await fetch("https://api.github.com/repos/FilenCloudDienste/filen-cli/releases/latest")
 		const releaseInfo: ReleaseInfo = await releaseInfoResponse.json()
 
 		const currentVersion = version
 		const publishedVersion = releaseInfo.tag_name
 
-		if (currentVersion === "0.0.0") {
-			if (verbose) out("Skipping updates in development environment")
-			return
-		}
-
-		if (currentVersion !== publishedVersion) {
+		let platformStr = "linux"
+		if (process.platform === "win32") platformStr = "win"
+		if (process.platform === "darwin") platformStr = "macos"
+		const downloadUrl = releaseInfo.assets.find(asset => asset.name.includes(platformStr) && asset.name.includes(process.arch))?.browser_download_url ?? undefined
+		if (downloadUrl !== undefined && currentVersion !== publishedVersion) {
 			if ((await prompt(`Update from ${currentVersion} to ${publishedVersion}? [y/N] `)).toLowerCase() === "y") {
-				await this.update(releaseInfo)
+				await this.update(downloadUrl, releaseInfo.tag_name)
 			}
 		} else {
 			if (verbose) out(`${currentVersion} is up to date.`)
 		}
 	}
 
-	private async update(releaseInfo: ReleaseInfo) {
-		let platformStr = "linux"
-		if (process.platform === "win32") platformStr = "win"
-		if (process.platform === "darwin") platformStr = "macos"
-		const url = releaseInfo.assets.find(asset => asset.name.includes(platformStr) && asset.name.includes(process.arch))!.browser_download_url
+	private async update(downloadUrl: string, publishedVersion: string) {
 		const selfApplicationFile = process.pkg === undefined ? __filename : process.argv[0]!
-		const downloadedFile = path.join(path.dirname(selfApplicationFile), `filen_update_${releaseInfo.tag_name}`)
+		const downloadedFile = path.join(path.dirname(selfApplicationFile), `filen_update_${publishedVersion}`)
 
 		out("Downloading update...")
-		await downloadFile(url, downloadedFile)
+		await downloadFile(downloadUrl, downloadedFile)
 
 		out("Installing update...")
 		if (process.platform === "win32") {
 			const commands = [
 				`del "${selfApplicationFile}"`,
 				`rename "${downloadedFile}" ${path.basename(selfApplicationFile)}`,
-				`echo "Successfully updated to ${releaseInfo.tag_name}"`,
+				`echo "Successfully updated to ${publishedVersion}"`,
 				"pause"
 			]
 			spawn("cmd.exe", ["/c", commands.join(" & ")], { shell: true, detached: true })
@@ -67,7 +67,7 @@ export class Updater {
 				`rm "${selfApplicationFile}"`,
 				`chmod +x "${downloadedFile}"`,
 				`mv "${downloadedFile}" "${selfApplicationFile}"`,
-				`echo "Successfully updated to ${releaseInfo.tag_name}"`,
+				`echo "Successfully updated to ${publishedVersion}"`,
 				"read -p \"Press enter to continue...\""
 			]
 			spawn("sh", ["-c", `${commands.join(" & ")}`], { detached: true })
