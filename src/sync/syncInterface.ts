d6ef7373ffb62451e9e479a20df46e6431618891
JupiterPi <jupiterpi@outlook.de>
2024-07-23 19:05:23 +0200

    feat: sync pairs registry (also via path), aliases

--- src/sync/syncInterface.ts
+++ src/sync/syncInterface.ts
@@ -1,9 +1,39 @@
 import FilenSDK from "@filen/sdk"
 import SyncWorker from "@filen/sync"
 import { randomUUID } from "node:crypto"
-import path from "path"
+import pathModule from "path"
 import { SyncMode, SyncPair } from "@filen/sync/dist/types"
-import { err, errExit } from "../interface/interface"
+import { err, errExit, out } from "../interface/interface"
+import fsModule, { PathLike } from "node:fs"
+import { exists, platformConfigPath } from "../util"
+
+export type RawSyncPair = {
+	local: string
+	remote: string
+	syncMode: SyncMode
+}
+
+const syncModes = [
+	"twoWay",
+	"localToCloud",
+	"localBackup",
+	"cloudToLocal",
+	"cloudBackup",
+]
+const syncModeMappings = new Map<string, SyncMode>([
+	[":twoWay:", "twoWay"],
+	[":tw:", "twoWay"],
+	[":localToCloud:", "localToCloud"],
+	[":ltc:", "localToCloud"],
+	[":localBackup:", "localBackup"],
+	[":lb:", "localBackup"],
+	[":cloudToLocal:", "cloudToLocal"],
+	[":ctl:", "cloudToLocal"],
+	[":cloudBackup:", "cloudBackup"],
+	[":cb:", "cloudBackup"],
+	["::", "twoWay"],
+	[":", "twoWay"],
+])
 
 /**
  * Provides the interface for syncing.
@@ -11,56 +41,34 @@ import { err, errExit } from "../interface/interface"
 export class SyncInterface {
 	private readonly filen
 
-	private readonly syncModes = new Map<string, SyncMode>([
-		[":twoWay:", "twoWay"],
-		[":tw:", "twoWay"],
-		[":localToCloud:", "localToCloud"],
-		[":ltc:", "localToCloud"],
-		[":localBackup:", "localBackup"],
-		[":lb:", "localBackup"],
-		[":cloudToLocal:", "cloudToLocal"],
-		[":ctl:", "cloudToLocal"],
-		[":cloudBackup:", "cloudBackup"],
-		[":cb:", "cloudBackup"],
-		["::", "twoWay"],
-		[":", "twoWay"],
-	])
+	private readonly defaultSyncPairsRegistry = pathModule.join(platformConfigPath(), "syncPairs.json")
 
 	constructor(filen: FilenSDK) {
 		this.filen = filen
 	}
 
 	public async invoke(locationsStr: string[]) {
-		const syncPairs: SyncPair[] = []
-		for (const str of locationsStr) {
-			let local
-			let remote
-			let syncMode: SyncMode | undefined = undefined
-			for (const syncModeMapping of this.syncModes.entries()) {
-				if (str.includes(syncModeMapping[0])) {
-					local = str.slice(0, str.lastIndexOf(syncModeMapping[0]))
-					remote = str.slice(str.lastIndexOf(syncModeMapping[0]) + syncModeMapping[0].length)
-					syncMode = syncModeMapping[1]
-					break
-				}
-			}
-			if (local === undefined || remote === undefined || syncMode === undefined) {
-				errExit("Apparently alias: " + str + " (not yet implemented)")
-			}
-			syncPairs.push({
-				name: `${local}:${remote}`,
+		const syncPairs = await this.resolveSyncPairs(locationsStr)
+		for (const syncPair of syncPairs) {
+			out(`Syncing ${syncPair.local} to ${syncPair.remote} (${syncPair.syncMode})...`)
+		}
+
+		const fullSyncPairs: SyncPair[] = []
+		for (const syncPair of syncPairs) {
+			fullSyncPairs.push({
+				name: `${syncPair.local}:${syncPair.remote}`,
 				uuid: randomUUID(),
-				localPath: local,
-				remotePath: remote,
-				remoteParentUUID: (await this.filen.fs().stat({ path: remote })).uuid,
-				mode: syncMode,
+				localPath: syncPair.local,
+				remotePath: syncPair.remote,
+				remoteParentUUID: (await this.filen.fs().stat({ path: syncPair.remote })).uuid,
+				mode: syncPair.syncMode,
 				excludeDotFiles: false,
 				paused: false
 			})
 		}
 		const worker = new SyncWorker({
-			syncPairs,
-			dbPath: path.join(__dirname, "db"),
+			syncPairs: fullSyncPairs,
+			dbPath: pathModule.join(__dirname, "db"),
 			sdkConfig: this.filen.config,
 			onMessage: msg => {
 				if (msg.type.includes("error")) err(msg.toString())
@@ -69,5 +77,68 @@ export class SyncInterface {
 			runOnce: true,
 		})
 		await worker.initialize()
+	}
+
+	private async resolveSyncPairs(locationsStr: string[]): Promise<RawSyncPair[]> {
+		if (locationsStr.length === 0) {
+			return (await this.getSyncPairsFromFile(this.defaultSyncPairsRegistry)).syncPairs
+		} else if (
+			locationsStr.length === 1
+			&& await exists(locationsStr[0]!)
+			&& !(await fsModule.promises.stat(locationsStr[0]!)).isDirectory()
+		) {
+			return (await this.getSyncPairsFromFile(locationsStr[0]!)).syncPairs
+		} else {
+			const syncPairs: RawSyncPair[] = []
+			for (const str of locationsStr) {
+				syncPairs.push(this.resolveSyncPairLiteral(str) ?? await this.resolveSyncPairAlias(str))
+			}
+			return syncPairs
+		}
+	}
+
+	private async getSyncPairsFromFile(path: PathLike): Promise<{ syncPairs: RawSyncPair[], aliases: Map<string, RawSyncPair> }> {
+		const syncPairs: RawSyncPair[] = []
+		const aliases = new Map<string, RawSyncPair>()
+
+		if (!await exists(path)) {
+			errExit(`You need to create ${path} or specify another sync pairs registry using \`filen sync <path>\`!`)
+		}
+		const file = JSON.parse((await fsModule.promises.readFile(path)).toString())
+		const exitTypeErr = () => errExit("Invalid sync pairs registry! Needs to be of type: {local: string, remote: string, syncMode: string, alias?: string}[]")
+		if (!Array.isArray(file)) exitTypeErr()
+		for (const obj of file) {
+			if (typeof obj.local !== "string") exitTypeErr()
+			if (typeof obj.remote !== "string") exitTypeErr()
+			const syncMode: string = typeof obj.syncMode === "string" ? obj.syncMode : "twoWay"
+			if (!syncModes.includes(syncMode)) exitTypeErr()
+			const syncPair = { local: obj.local, remote: obj.remote, syncMode: syncMode as SyncMode }
+			syncPairs.push(syncPair)
+			if (typeof obj.alias === "string") aliases.set(obj.alias, syncPair)
+		}
+
+		return { syncPairs, aliases }
+	}
+
+	private resolveSyncPairLiteral(str: string): RawSyncPair | undefined {
+		for (const syncModeMapping of syncModeMappings.entries()) {
+			if (str.includes(syncModeMapping[0])) {
+				return {
+					local: str.slice(0, str.lastIndexOf(syncModeMapping[0])),
+					remote: str.slice(str.lastIndexOf(syncModeMapping[0]) + syncModeMapping[0].length),
+					syncMode: syncModeMapping[1]
+				}
+			}
+		}
+	}
+
+	private _aliases?: Map<string, RawSyncPair>
+	private async resolveSyncPairAlias(str: string): Promise<RawSyncPair> {
+		if (this._aliases === undefined) {
+			this._aliases = (await this.getSyncPairsFromFile(this.defaultSyncPairsRegistry)).aliases
+		}
+		const syncPair = this._aliases.get(str)
+		if (syncPair === undefined) errExit("Unknown sync pair alias: " + str)
+		return syncPair
 	}
 }
