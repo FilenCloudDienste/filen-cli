ce7172054c6bf2277fc86c7940361185c0dfc50c
JupiterPi <jupiterpi@outlook.de>
2024-07-28 16:45:54 +0200

    fix: fix packaging for cloud builds

--- packageAllPlatforms.mjs
+++ packageAllPlatforms.mjs
@@ -2,18 +2,17 @@ import { spawn } from "child_process"
 import pkg from "@yao-pkg/pkg"
 import * as fs from "node:fs"
 
+const local = process.argv.includes("dev")
 const bundleFile = "dist/bundle.js"
 
 const targets = [
 	{ name: "win-x64", pkgTarget: "win32-x64", dependencies: [ "@parcel/watcher-win32-x64" ] },
-	// { name: "win-arm64", pkgTarget: "win32-arm64", parcelWatcherTarget: "win32-arm64" },
-	{ name: "linux-x64", pkgTarget: "linux-x64", dependencies: [ "@parcel/watcher-linux-x64-glibc" ] },
-	// { name: "linux-arm64-glibc", pkgTarget: "linux-arm64", parcelWatcherTarget: "linux-arm64-glibc" },
-	// { name: "linux-x64-musl", pkgTarget: "linux-x64", dependencies: [ "@parcel/watcher-linux-x64-musl" ] },
-	// { name: "linux-arm64-musl", pkgTarget: "linux-arm64", parcelWatcherTarget: "linux-arm64-musl " },
+	{ name: "win-arm64", pkgTarget: "win32-arm64", dependencies: [ "@parcel/watcher-win32-arm64" ] },
+	{ name: "linux-x64", pkgTarget: "linux-x64", dependencies: [ "@parcel/watcher-linux-x64-glibc", "@parcel/watcher-linux-x64-musl" ] },
+	{ name: "linux-arm64", pkgTarget: "linux-arm64", dependencies: [ "@parcel/watcher-linux-arm64-glibc", "@parcel/watcher-linux-arm64-musl" ] },
 	{ name: "macos-x64", pkgTarget: "macos-x64", dependencies: [ "@parcel/watcher-darwin-x64" ] },
-	// { name: "macos-arm64", pkgTarget: "macos-arm64", parcelWatcherTarget: "darwin-arm64" },
-]
+	{ name: "macos-arm64", pkgTarget: "macos-arm64", dependencies: [ "@parcel/watcher-darwin-arm64" ] },
+].filter(t => !local || t.name.includes(process.arch))
 
 // install temporary dependencies
 await new Promise((resolve, _) => {
@@ -24,6 +23,7 @@ await new Promise((resolve, _) => {
 
 const placeholderStart = "/* INJECTED DEPENDENCIES PLACEHOLDER >>>>> */"
 const placeholderEnd = "/* <<<<< INJECTED DEPENDENCIES PLACEHOLDER */"
+const placeholderRegex = /\/\* INJECTED DEPENDENCIES PLACEHOLDER >>>>> \*\/.*\/\* <<<<< INJECTED DEPENDENCIES PLACEHOLDER \*\//s
 
 // prepare bundle file
 let bundle = fs.readFileSync(bundleFile).toString()
@@ -36,21 +36,21 @@ for (const target of targets) {
 
 	// inject require statements
 	let bundle = fs.readFileSync(bundleFile).toString()
-	bundle = bundle.replace(
-		/\/\* INJECTED DEPENDENCIES PLACEHOLDER >>>>> \*\/.*\/\* <<<<< INJECTED DEPENDENCIES PLACEHOLDER \*\//s,
-		`${placeholderStart}\n${target.dependencies.map(d => `if (false) require("${d}");`).join("\n")}\n${placeholderEnd}`
-	)
+	bundle = bundle.replace(placeholderRegex, `${placeholderStart}\n${target.dependencies.map(d => `if (false) require("${d}");`).join("\n")}\n${placeholderEnd}`)
 	fs.writeFileSync(bundleFile, bundle)
 
 	await pkg.exec(`-t ${target.pkgTarget} -o dist/filen-cli-${target.name} dist/bundle.js`.split(" "))
 }
 
-// remove bundle.js
-fs.rmSync(bundleFile)
+if (local) {
+	// remove require statements
+	bundle = fs.readFileSync(bundleFile).toString().replace(placeholderRegex, "")
+	fs.writeFileSync(bundleFile, bundle)
 
-// remove temporary dependencies
-await new Promise((resolve, _) => {
-	spawn("npm", ["remove", ...targets.flatMap(t => t.dependencies)], { shell: true })
-		.on("error", err => console.error(err))
-		.on("close", () => resolve())
-})
+	// remove temporary dependencies
+	await new Promise((resolve, _) => {
+		spawn("npm", ["remove", ...targets.flatMap(t => t.dependencies)], { shell: true })
+			.on("error", err => console.error(err))
+			.on("close", () => resolve())
+	})
+}
