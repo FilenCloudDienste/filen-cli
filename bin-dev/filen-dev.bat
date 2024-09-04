daf26bd83ef993469100476d594628e1f020835a
JupiterPi <jupiterpi@outlook.de>
2024-09-05 00:17:03 +0200

    fix: copy @parcel/watcher-win32-x64 to correct node_modules for dev environment

--- bin-dev/filen-dev.bat
+++ bin-dev/filen-dev.bat
@@ -1,2 +1,3 @@
 @echo off
+if not exist %~dp0\..\node_modules\@parcel\watcher-win32-x64 move %~dp0\..\node_modules\@parcel\watcher\node_modules\@parcel\watcher-win32-x64 %~dp0\..\node_modules\@parcel
 node %~dp0\..\dist\bundle.js --dev %*
