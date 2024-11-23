6ffd47a2c122b034dbd0e4bd8e3c7882cd4d2f5a
JupiterPi <hello@jupiterpi.de>
2024-11-23 16:39:33 +0100

    workflow: make filen-dev.bat work on arm64

--- bin-dev/filen-dev.bat
+++ bin-dev/filen-dev.bat
@@ -1,3 +1,9 @@
 @echo off
-if not exist %~dp0\..\node_modules\@parcel\watcher-win32-x64 move %~dp0\..\node_modules\@parcel\watcher\node_modules\@parcel\watcher-win32-x64 %~dp0\..\node_modules\@parcel
+if exist "%SYSTEMDRIVE%\Program Files (x86)\" (
+    :: is 64-bit
+    if not exist %~dp0\..\node_modules\@parcel\watcher-win32-x64 move %~dp0\..\node_modules\@parcel\watcher\node_modules\@parcel\watcher-win32-x64 %~dp0\..\node_modules\@parcel
+) else (
+    :: is 32-bit
+    if not exist %~dp0\..\node_modules\@parcel\watcher-win32-arm64 move %~dp0\..\node_modules\@parcel\watcher\node_modules\@parcel\watcher-win32-arm64 %~dp0\..\node_modules\@parcel
+)
 node %~dp0\..\dist\bundle.js --dev %*
