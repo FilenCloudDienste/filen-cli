21d76b3f8ddaa326184b1bd48d843329a7a6adf2
JupiterPi <hello@jupiterpi.de>
2025-07-13 21:04:03 +0200

    ci(tests): raise test timeout

--- src/test/vitest.config.ts
+++ src/test/vitest.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
     test: {
         dir: "src/",
-        testTimeout: 60000,
-        hookTimeout: 60000,
+        testTimeout: 300000,
+        hookTimeout: 300000,
         globalSetup: "./src/test/setup.ts",
     }
 })
